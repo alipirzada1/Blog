cca7bc1154b76def1f70921b74e582e941cd0047
Ali Pirzada <alipirzada.khan@gmail.com>
2021-01-08 22:29:07 +0500

    add ApprovalCard Component

--- src/RandomPost.js
+++ src/RandomPost.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 let PostArr = [
   "Wow, Great Post",
   "Good read, thanks!",
--- src/index.js
+++ src/index.js
@@ -2,25 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        date={new Date().toLocaleString()}
-        avatar={faker.image.image()}
-      />
-      <CommentDetail
-        author="Alex"
-        date={new Date().toLocaleString()}
-        avatar={faker.image.image()}
-      />
-      <CommentDetail
-        author="Jane"
-        date={new Date().toLocaleString()}
-        avatar={faker.image.image()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          date={new Date().toLocaleString()}
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          date={new Date().toLocaleString()}
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          date={new Date().toLocaleString()}
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          <p>Are you sure you want to do this?</p>
+        </div>
+      </ApprovalCard>
     </div>
   );
 };
